bf5a451946d62fa17947de4001a7db25efcf0f12
Ahtem <a.sitjalilov@ideas-world.com>
2022-01-23 02:29:19 +0300

    config

--- src/main.js
+++ src/main.js
@@ -18,10 +18,6 @@ if (token) {
 
 Vue.config.productionTip = false
 
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/'
-}
-
 new Vue({
     vuetify,
     router,
