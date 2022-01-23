cc60839bc3c196695dfba64e79f1cc1dfed05ed2
Ahtem <a.sitjalilov@ideas-world.com>
2022-01-23 23:46:24 +0300

    edit router

--- src/router/main.js
+++ src/router/main.js
@@ -1,15 +1,18 @@
 import News from '@/components/mainScreens/News.vue'
 import Events from '@/components/mainScreens/Events.vue'
 import Users from '@/components/mainScreens/Users.vue'
-
-import Ad from '@/components/Books/Ad'
-import MyBooks from '@/components/Books/MyBooks'
-import NewBook from '@/components/Books/NewBook'
 import Login from '@/components/Auth/Login'
 import Registration from '@/components/Auth/Registration'
-import Orders from '@/components/User/Orders'
 
 export default [{
+        path: "/login",
+        component: Login
+    },
+    {
+        path: "/registration",
+        component: Registration
+    },
+    {
         path: "/news",
         component: News,
     },
@@ -20,31 +23,5 @@ export default [{
     {
         path: "/users",
         component: Users
-    },
-
-
-    {
-        path: "/ad/:id",
-        component: Ad
-    },
-    {
-        path: "/myBooks",
-        component: MyBooks
-    },
-    {
-        path: "/newbook",
-        component: NewBook
-    },
-    {
-        path: "/login",
-        component: Login
-    },
-    {
-        path: "/registration",
-        component: Registration
-    },
-    {
-        path: "/orders",
-        component: Orders
-    },
+    }
 ]
